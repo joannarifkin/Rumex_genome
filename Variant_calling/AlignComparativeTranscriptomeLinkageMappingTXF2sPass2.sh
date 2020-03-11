4ca4b5efdb307710acf38c7b49660e9355650463
joannarifkin <joannarifkin@gmail.com>
2020-03-11 15:31:56 -0400

    Adding scripts for read alignment

---
+++ Variant_calling/AlignComparativeTranscriptomeLinkageMappingTXF2sPass2.sh
@@ -0,0 +1,109 @@
+for i in "A1.TX1F" "A10.TX76F" "A11.TX84M" "A12.TX93F" "A2.TX9F" "A3.TX17F" "A4.TX25M" "A5.TX33M" "A6.TX42M" "A7.TX51F" "A8.TX59F" "A9.TX68M" "B1.TX2M" "B10.TX77M" "B11.TX86F" "B12.TX94M" "B2.TX10F" "B3.TX18M" "B4.TX26M" "B5.TX34M" "B6.TX43M" "B7.TX52F" "B8.TX61M" "B9.TX69F" "C1.TX3M" "C10.TX78M" "C11.TX87F" "C12.TX95F" "C2.TX11F" "C3.TX19M" "C4.TX27M" "C5.TX35F" "C6.TX44M" "C7.TX53F" "C8.TX62F" "C9.TX70M" "D1.TX4M" "D10.TX79F" "D11.TX88M" "D12.TX96F" "D2.TX12F" "D3.TX20F" "D4.TX28M" "D5.TX36M" "D6.TX45M" "D7.TX54M" "D8.TX63F" "D9.TX71M" "E1.TX5F" "E10.TX80M" "E11.TX89F" "E12.TX97M" "E2.TX13M" "E3.TX21M" "E4.TX29M" "E5.TX37M" "E6.TX46M" "E7.TX55F" "E8.TX64F" "E9.TX72M" "F1.TX6F" "F10.TX81F" "F11.TX90M" "F12.TX98F" "F2.TX14F" "F3.TX22F" "F4.TX30F" "F5.TX38M" "F6.TX47M" "F7.TX56M" "F8.TX65M" "F9.TX73F" "G1.TX7M" "G10.TX82F" "G11.TX91F" "G12.TX99M" "G2.TX15F" "G3.TX23F" "G4.TX31M" "G5.TX39F" "G6.TX49M" "G7.TX57F" "G8.TX66F" "G9.TX74M" "H1.TX8F" "H10.TX83M" "H11.TX92M" "H12.TX100M" "H2.TX16F" "H3.TX24M" "H4.TX32M" "H5.TX40M" "H6.TX50M" "H7.TX58M" "H8.TX67M" "H9.TX75F"
+
+do
+
+STAR --genomeDir /ohta/joanna.rifkin/Genomes/Hi-C \
+--readFilesIn /ohta/Rumex/RNASEQmapping/NS.1095.00*.NEBNext_dual_i7_*---NEBNext_dual_i5_$i\_R1.fastq.gz \
+/ohta/Rumex/RNASEQmapping/NS.1095.00*.NEBNext_dual_i7_*---NEBNext_dual_i5_$i\_R2.fastq.gz \
+ --readFilesCommand zcat \
+ --sjdbFileChrStartEnd \
+/ohta/joanna.rifkin/Alignments/STAR/ComparativeTranscriptomeMap/TX/Pass1/11-27_Pass1_H9.TX75FSJ.out.tab \
+/ohta/joanna.rifkin/Alignments/STAR/ComparativeTranscriptomeMap/TX/Pass1/11-27_Pass1_H8.TX67MSJ.out.tab \
+/ohta/joanna.rifkin/Alignments/STAR/ComparativeTranscriptomeMap/TX/Pass1/11-27_Pass1_H7.TX58MSJ.out.tab \
+/ohta/joanna.rifkin/Alignments/STAR/ComparativeTranscriptomeMap/TX/Pass1/11-27_Pass1_H6.TX50MSJ.out.tab \
+/ohta/joanna.rifkin/Alignments/STAR/ComparativeTranscriptomeMap/TX/Pass1/11-27_Pass1_H5.TX40MSJ.out.tab \
+/ohta/joanna.rifkin/Alignments/STAR/ComparativeTranscriptomeMap/TX/Pass1/11-27_Pass1_H4.TX32MSJ.out.tab \
+/ohta/joanna.rifkin/Alignments/STAR/ComparativeTranscriptomeMap/TX/Pass1/11-27_Pass1_H3.TX24MSJ.out.tab \
+/ohta/joanna.rifkin/Alignments/STAR/ComparativeTranscriptomeMap/TX/Pass1/11-27_Pass1_H2.TX16FSJ.out.tab \
+/ohta/joanna.rifkin/Alignments/STAR/ComparativeTranscriptomeMap/TX/Pass1/11-27_Pass1_H12.TX100MSJ.out.tab \
+/ohta/joanna.rifkin/Alignments/STAR/ComparativeTranscriptomeMap/TX/Pass1/11-27_Pass1_H11.TX92MSJ.out.tab \
+/ohta/joanna.rifkin/Alignments/STAR/ComparativeTranscriptomeMap/TX/Pass1/11-27_Pass1_H10.TX83MSJ.out.tab \
+/ohta/joanna.rifkin/Alignments/STAR/ComparativeTranscriptomeMap/TX/Pass1/11-27_Pass1_H1.TX8FSJ.out.tab \
+/ohta/joanna.rifkin/Alignments/STAR/ComparativeTranscriptomeMap/TX/Pass1/11-27_Pass1_G9.TX74MSJ.out.tab \
+/ohta/joanna.rifkin/Alignments/STAR/ComparativeTranscriptomeMap/TX/Pass1/11-27_Pass1_G8.TX66FSJ.out.tab \
+/ohta/joanna.rifkin/Alignments/STAR/ComparativeTranscriptomeMap/TX/Pass1/11-27_Pass1_G7.TX57FSJ.out.tab \
+/ohta/joanna.rifkin/Alignments/STAR/ComparativeTranscriptomeMap/TX/Pass1/11-27_Pass1_G6.TX49MSJ.out.tab \
+/ohta/joanna.rifkin/Alignments/STAR/ComparativeTranscriptomeMap/TX/Pass1/11-27_Pass1_G5.TX39FSJ.out.tab \
+/ohta/joanna.rifkin/Alignments/STAR/ComparativeTranscriptomeMap/TX/Pass1/11-27_Pass1_G4.TX31MSJ.out.tab \
+/ohta/joanna.rifkin/Alignments/STAR/ComparativeTranscriptomeMap/TX/Pass1/11-27_Pass1_G3.TX23FSJ.out.tab \
+/ohta/joanna.rifkin/Alignments/STAR/ComparativeTranscriptomeMap/TX/Pass1/11-27_Pass1_G2.TX15FSJ.out.tab \
+/ohta/joanna.rifkin/Alignments/STAR/ComparativeTranscriptomeMap/TX/Pass1/11-27_Pass1_G12.TX99MSJ.out.tab \
+/ohta/joanna.rifkin/Alignments/STAR/ComparativeTranscriptomeMap/TX/Pass1/11-27_Pass1_G11.TX91FSJ.out.tab \
+/ohta/joanna.rifkin/Alignments/STAR/ComparativeTranscriptomeMap/TX/Pass1/11-27_Pass1_G10.TX82FSJ.out.tab \
+/ohta/joanna.rifkin/Alignments/STAR/ComparativeTranscriptomeMap/TX/Pass1/11-27_Pass1_G1.TX7MSJ.out.tab \
+/ohta/joanna.rifkin/Alignments/STAR/ComparativeTranscriptomeMap/TX/Pass1/11-27_Pass1_F9.TX73FSJ.out.tab \
+/ohta/joanna.rifkin/Alignments/STAR/ComparativeTranscriptomeMap/TX/Pass1/11-27_Pass1_F8.TX65MSJ.out.tab \
+/ohta/joanna.rifkin/Alignments/STAR/ComparativeTranscriptomeMap/TX/Pass1/11-27_Pass1_F7.TX56MSJ.out.tab \
+/ohta/joanna.rifkin/Alignments/STAR/ComparativeTranscriptomeMap/TX/Pass1/11-27_Pass1_F6.TX47MSJ.out.tab \
+/ohta/joanna.rifkin/Alignments/STAR/ComparativeTranscriptomeMap/TX/Pass1/11-27_Pass1_F5.TX38MSJ.out.tab \
+/ohta/joanna.rifkin/Alignments/STAR/ComparativeTranscriptomeMap/TX/Pass1/11-27_Pass1_F4.TX30FSJ.out.tab \
+/ohta/joanna.rifkin/Alignments/STAR/ComparativeTranscriptomeMap/TX/Pass1/11-27_Pass1_F3.TX22FSJ.out.tab \
+/ohta/joanna.rifkin/Alignments/STAR/ComparativeTranscriptomeMap/TX/Pass1/11-27_Pass1_F2.TX14FSJ.out.tab \
+/ohta/joanna.rifkin/Alignments/STAR/ComparativeTranscriptomeMap/TX/Pass1/11-27_Pass1_F12.TX98FSJ.out.tab \
+/ohta/joanna.rifkin/Alignments/STAR/ComparativeTranscriptomeMap/TX/Pass1/11-27_Pass1_F11.TX90MSJ.out.tab \
+/ohta/joanna.rifkin/Alignments/STAR/ComparativeTranscriptomeMap/TX/Pass1/11-27_Pass1_F10.TX81FSJ.out.tab \
+/ohta/joanna.rifkin/Alignments/STAR/ComparativeTranscriptomeMap/TX/Pass1/11-27_Pass1_F1.TX6FSJ.out.tab \
+/ohta/joanna.rifkin/Alignments/STAR/ComparativeTranscriptomeMap/TX/Pass1/11-27_Pass1_E9.TX72MSJ.out.tab \
+/ohta/joanna.rifkin/Alignments/STAR/ComparativeTranscriptomeMap/TX/Pass1/11-27_Pass1_E8.TX64FSJ.out.tab \
+/ohta/joanna.rifkin/Alignments/STAR/ComparativeTranscriptomeMap/TX/Pass1/11-27_Pass1_E7.TX55FSJ.out.tab \
+/ohta/joanna.rifkin/Alignments/STAR/ComparativeTranscriptomeMap/TX/Pass1/11-27_Pass1_E6.TX46MSJ.out.tab \
+/ohta/joanna.rifkin/Alignments/STAR/ComparativeTranscriptomeMap/TX/Pass1/11-27_Pass1_E5.TX37MSJ.out.tab \
+/ohta/joanna.rifkin/Alignments/STAR/ComparativeTranscriptomeMap/TX/Pass1/11-27_Pass1_E4.TX29MSJ.out.tab \
+/ohta/joanna.rifkin/Alignments/STAR/ComparativeTranscriptomeMap/TX/Pass1/11-27_Pass1_E3.TX21MSJ.out.tab \
+/ohta/joanna.rifkin/Alignments/STAR/ComparativeTranscriptomeMap/TX/Pass1/11-27_Pass1_E2.TX13MSJ.out.tab \
+/ohta/joanna.rifkin/Alignments/STAR/ComparativeTranscriptomeMap/TX/Pass1/11-27_Pass1_E12.TX97MSJ.out.tab \
+/ohta/joanna.rifkin/Alignments/STAR/ComparativeTranscriptomeMap/TX/Pass1/11-27_Pass1_E11.TX89FSJ.out.tab \
+/ohta/joanna.rifkin/Alignments/STAR/ComparativeTranscriptomeMap/TX/Pass1/11-27_Pass1_E10.TX80MSJ.out.tab \
+/ohta/joanna.rifkin/Alignments/STAR/ComparativeTranscriptomeMap/TX/Pass1/11-27_Pass1_E1.TX5FSJ.out.tab \
+/ohta/joanna.rifkin/Alignments/STAR/ComparativeTranscriptomeMap/TX/Pass1/11-27_Pass1_D9.TX71MSJ.out.tab \
+/ohta/joanna.rifkin/Alignments/STAR/ComparativeTranscriptomeMap/TX/Pass1/11-27_Pass1_D8.TX63FSJ.out.tab \
+/ohta/joanna.rifkin/Alignments/STAR/ComparativeTranscriptomeMap/TX/Pass1/11-27_Pass1_D7.TX54MSJ.out.tab \
+/ohta/joanna.rifkin/Alignments/STAR/ComparativeTranscriptomeMap/TX/Pass1/11-27_Pass1_D6.TX45MSJ.out.tab \
+/ohta/joanna.rifkin/Alignments/STAR/ComparativeTranscriptomeMap/TX/Pass1/11-27_Pass1_D5.TX36MSJ.out.tab \
+/ohta/joanna.rifkin/Alignments/STAR/ComparativeTranscriptomeMap/TX/Pass1/11-27_Pass1_D4.TX28MSJ.out.tab \
+/ohta/joanna.rifkin/Alignments/STAR/ComparativeTranscriptomeMap/TX/Pass1/11-27_Pass1_D3.TX20FSJ.out.tab \
+/ohta/joanna.rifkin/Alignments/STAR/ComparativeTranscriptomeMap/TX/Pass1/11-27_Pass1_D2.TX12FSJ.out.tab \
+/ohta/joanna.rifkin/Alignments/STAR/ComparativeTranscriptomeMap/TX/Pass1/11-27_Pass1_D12.TX96FSJ.out.tab \
+/ohta/joanna.rifkin/Alignments/STAR/ComparativeTranscriptomeMap/TX/Pass1/11-27_Pass1_D11.TX88MSJ.out.tab \
+/ohta/joanna.rifkin/Alignments/STAR/ComparativeTranscriptomeMap/TX/Pass1/11-27_Pass1_D10.TX79FSJ.out.tab \
+/ohta/joanna.rifkin/Alignments/STAR/ComparativeTranscriptomeMap/TX/Pass1/11-27_Pass1_D1.TX4MSJ.out.tab \
+/ohta/joanna.rifkin/Alignments/STAR/ComparativeTranscriptomeMap/TX/Pass1/11-27_Pass1_C9.TX70MSJ.out.tab \
+/ohta/joanna.rifkin/Alignments/STAR/ComparativeTranscriptomeMap/TX/Pass1/11-27_Pass1_C8.TX62FSJ.out.tab \
+/ohta/joanna.rifkin/Alignments/STAR/ComparativeTranscriptomeMap/TX/Pass1/11-27_Pass1_C7.TX53FSJ.out.tab \
+/ohta/joanna.rifkin/Alignments/STAR/ComparativeTranscriptomeMap/TX/Pass1/11-27_Pass1_C6.TX44MSJ.out.tab \
+/ohta/joanna.rifkin/Alignments/STAR/ComparativeTranscriptomeMap/TX/Pass1/11-27_Pass1_C5.TX35FSJ.out.tab \
+/ohta/joanna.rifkin/Alignments/STAR/ComparativeTranscriptomeMap/TX/Pass1/11-27_Pass1_C4.TX27MSJ.out.tab \
+/ohta/joanna.rifkin/Alignments/STAR/ComparativeTranscriptomeMap/TX/Pass1/11-27_Pass1_C3.TX19MSJ.out.tab \
+/ohta/joanna.rifkin/Alignments/STAR/ComparativeTranscriptomeMap/TX/Pass1/11-27_Pass1_C2.TX11FSJ.out.tab \
+/ohta/joanna.rifkin/Alignments/STAR/ComparativeTranscriptomeMap/TX/Pass1/11-27_Pass1_C12.TX95FSJ.out.tab \
+/ohta/joanna.rifkin/Alignments/STAR/ComparativeTranscriptomeMap/TX/Pass1/11-27_Pass1_C11.TX87FSJ.out.tab \
+/ohta/joanna.rifkin/Alignments/STAR/ComparativeTranscriptomeMap/TX/Pass1/11-27_Pass1_C10.TX78MSJ.out.tab \
+/ohta/joanna.rifkin/Alignments/STAR/ComparativeTranscriptomeMap/TX/Pass1/11-27_Pass1_C1.TX3MSJ.out.tab \
+/ohta/joanna.rifkin/Alignments/STAR/ComparativeTranscriptomeMap/TX/Pass1/11-27_Pass1_B9.TX69FSJ.out.tab \
+/ohta/joanna.rifkin/Alignments/STAR/ComparativeTranscriptomeMap/TX/Pass1/11-27_Pass1_B8.TX61MSJ.out.tab \
+/ohta/joanna.rifkin/Alignments/STAR/ComparativeTranscriptomeMap/TX/Pass1/11-27_Pass1_B7.TX52FSJ.out.tab \
+/ohta/joanna.rifkin/Alignments/STAR/ComparativeTranscriptomeMap/TX/Pass1/11-27_Pass1_B6.TX43MSJ.out.tab \
+/ohta/joanna.rifkin/Alignments/STAR/ComparativeTranscriptomeMap/TX/Pass1/11-27_Pass1_B5.TX34MSJ.out.tab \
+/ohta/joanna.rifkin/Alignments/STAR/ComparativeTranscriptomeMap/TX/Pass1/11-27_Pass1_B4.TX26MSJ.out.tab \
+/ohta/joanna.rifkin/Alignments/STAR/ComparativeTranscriptomeMap/TX/Pass1/11-27_Pass1_B3.TX18MSJ.out.tab \
+/ohta/joanna.rifkin/Alignments/STAR/ComparativeTranscriptomeMap/TX/Pass1/11-27_Pass1_B2.TX10FSJ.out.tab \
+/ohta/joanna.rifkin/Alignments/STAR/ComparativeTranscriptomeMap/TX/Pass1/11-27_Pass1_B12.TX94MSJ.out.tab \
+/ohta/joanna.rifkin/Alignments/STAR/ComparativeTranscriptomeMap/TX/Pass1/11-27_Pass1_B11.TX86FSJ.out.tab \
+/ohta/joanna.rifkin/Alignments/STAR/ComparativeTranscriptomeMap/TX/Pass1/11-27_Pass1_B10.TX77MSJ.out.tab \
+/ohta/joanna.rifkin/Alignments/STAR/ComparativeTranscriptomeMap/TX/Pass1/11-27_Pass1_B1.TX2MSJ.out.tab \
+/ohta/joanna.rifkin/Alignments/STAR/ComparativeTranscriptomeMap/TX/Pass1/11-27_Pass1_A9.TX68MSJ.out.tab \
+/ohta/joanna.rifkin/Alignments/STAR/ComparativeTranscriptomeMap/TX/Pass1/11-27_Pass1_A8.TX59FSJ.out.tab \
+/ohta/joanna.rifkin/Alignments/STAR/ComparativeTranscriptomeMap/TX/Pass1/11-27_Pass1_A7.TX51FSJ.out.tab \
+/ohta/joanna.rifkin/Alignments/STAR/ComparativeTranscriptomeMap/TX/Pass1/11-27_Pass1_A6.TX42MSJ.out.tab \
+/ohta/joanna.rifkin/Alignments/STAR/ComparativeTranscriptomeMap/TX/Pass1/11-27_Pass1_A5.TX33MSJ.out.tab \
+/ohta/joanna.rifkin/Alignments/STAR/ComparativeTranscriptomeMap/TX/Pass1/11-27_Pass1_A4.TX25MSJ.out.tab \
+/ohta/joanna.rifkin/Alignments/STAR/ComparativeTranscriptomeMap/TX/Pass1/11-27_Pass1_A3.TX17FSJ.out.tab \
+/ohta/joanna.rifkin/Alignments/STAR/ComparativeTranscriptomeMap/TX/Pass1/11-27_Pass1_A2.TX9FSJ.out.tab \
+/ohta/joanna.rifkin/Alignments/STAR/ComparativeTranscriptomeMap/TX/Pass1/11-27_Pass1_A12.TX93FSJ.out.tab \
+/ohta/joanna.rifkin/Alignments/STAR/ComparativeTranscriptomeMap/TX/Pass1/11-27_Pass1_A11.TX84MSJ.out.tab \
+/ohta/joanna.rifkin/Alignments/STAR/ComparativeTranscriptomeMap/TX/Pass1/11-27_Pass1_A10.TX76FSJ.out.tab \
+/ohta/joanna.rifkin/Alignments/STAR/ComparativeTranscriptomeMap/TX/Pass1/11-27_Pass1_A1.TX1FSJ.out.tab \
+ --outFileNamePrefix /ohta/joanna.rifkin/Alignments/STAR/ComparativeTranscriptomeMap/TX/Pass2/11-28_Pass2_$i \
+--runThreadN 16
+
+done
--- Variant_calling/README.md
+++ Variant_calling/README.md
@@ -1,2 +1,4 @@
 # Variant_calling
  
+Index_genome.txt #Script to index our draft genome assembly for use with STAR
+AlignComparativeTranscriptomeLinkageMapping*F2sPass* #Scripts to align transcriptome reads to genome using STAR in 2-pass mode
