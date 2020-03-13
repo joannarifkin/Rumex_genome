c77c6777214e5a3f589f09d453d398ea368272d8
joannarifkin <joannarifkin@gmail.com>
2020-03-13 11:40:11 -0400

    Added LDHat scripts

---
+++ LDHat/README.md
@@ -0,0 +1,3 @@
+# LDHat
+Generate_\*_loc_sites_files.sh # Scripts to convert vcfs into input for LDHat for XY and XYY cytotypes
+XYlik_LDHat.sh # Scripts to run LDHat for XY and XYY cytotypes
---
+++ LDHat/XYlik_LDHat.sh
@@ -0,0 +1,8 @@
+# for LG in 7 10 3 5 8
+do
+
+rm new_lk.txt rates.txt bounds.txt res.txt 
+/ohta/felix.beaudry/scripts/LDhat-master/complete -n 54 -theta 0.006479939 -rhomax 100 -n_pts 101 
+cp new_lk.txt lk_XYY.LG${LG}.LDhat.txt
+
+done
